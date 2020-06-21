c56db675e92411a3032cca32bd832f238ca25210
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-21 20:41:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 # empty board
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["X"," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
